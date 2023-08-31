06b06f04cf7a65421e3461a3fc9dba24f0222178
FiodhyAN <ardhito917@gmail.com>
2023-08-31 15:22:38 +0700

    add change password

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import psycopg2
 from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from auth import register, login
+from auth import register, login, changePW
 
 app = Flask(__name__)
 CORS(app)
@@ -18,6 +18,10 @@ def registration_route():
 @app.route("/login", methods=["POST"])
 def login_route():
     return login()
+
+@app.route("/change-password", methods=["PUT"])
+def change_password_route():
+    return changePW()
 
 
 if __name__ == "__main__":
--- auth.py
+++ auth.py
@@ -32,7 +32,7 @@ def register():
             conn.commit()
             cur.close()
             conn.close()
-            return jsonify({"message": "Success"}), 200
+            return jsonify({"message": "Registration Successful Please Login"}), 200
     else:
         return jsonify({"message": "Registration failed wrong method"}), 400
     
@@ -61,3 +61,33 @@ def login():
             return jsonify({"message": "Invalid email or password"}), 400
     else:
         return jsonify({"message": "Login failed wrong method"}), 400
+    
+def changePW():
+    email = request.form["email"]
+    password = request.form["password"]
+    confirmPassword = request.form["confirmPassword"]
+
+    if request.method == "PUT" and email and password and confirmPassword:
+        conn = db_conn()
+        cur = conn.cursor()
+        
+        cur.execute(''' SELECT * FROM users WHERE email = %s ''', (email,))
+        user = cur.fetchone()
+        
+        if user:
+            if password == confirmPassword:
+                if len(confirmPassword) < 6 or re.search('[0-9]', confirmPassword) is None:
+                    return jsonify({"message": "Password must be at least 6 characters long and contain a number"}), 400
+                else:
+                    password = bcrypt.generate_password_hash(confirmPassword).decode('utf-8')
+                    cur.execute(''' UPDATE users SET password = %s WHERE email = %s ''', (password, email))
+                    conn.commit()
+                    cur.close()
+                    conn.close()
+                    return jsonify({"message": "Successfully change password"}), 200
+            else:
+                return jsonify({"message": "Password and Confirm Password must be same"}), 400
+        else:
+            return jsonify({"message": "Email not found"}), 400
+    else:
+        return jsonify({"message": "Change Password failed wrong method"}), 400
