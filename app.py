0727e4b3d2ce9a138825ce6bb577b9ab355194c4
FiodhyAN <ardhito917@gmail.com>
2023-09-03 21:52:20 +0700

    get history and add column for table

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import psycopg2
-from flask import Flask, request, jsonify
+import mimetypes
+from flask import Flask, request, jsonify, send_file
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from auth import register, login, changePW
-from reports import create_report
+from reports import create_report, get_reports
 
 app = Flask(__name__)
 CORS(app)
@@ -27,6 +28,20 @@ def change_password_route():
 @app.route("/reports/store", methods=["POST"])
 def create_report_route():
     return create_report()
+
+@app.route("/reports", methods=["GET"])
+def get_reports_route():
+    return get_reports()
+
+@app.route("/images/<filename>")
+def get_image(filename):
+    file_path = f"./images/{filename}"
+    mimetype, _ = mimetypes.guess_type(file_path)
+    
+    if mimetype is None:
+        mimetype = 'application/octet-stream'  # Default to binary if mimetype cannot be determined
+
+    return send_file(file_path, mimetype=mimetype)
 
 
 if __name__ == "__main__":
--- reports.py
+++ reports.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from werkzeug.utils import secure_filename
 import psycopg2
 import os
+import random
 from datetime import datetime
 
 UPLOAD_FOLDER = "./images"
@@ -15,24 +16,63 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def create_report():
+    jenis_pengaduan = request.form["jenis_pengaduan"]
+    timestamp = int(datetime.now().timestamp())
+    random_number = random.randint(100, 999)
+    if jenis_pengaduan == "Perbaikan":
+        no_laporan = f"PRB_{timestamp}_{random_number}"
+    else:
+        no_laporan = f"PNG_{timestamp}_{random_number}"
     user_id = request.form["user_id"]
     file = request.files["image_url"]
     nama_pengadu = request.form["nama_pengadu"]
-    jenis_pengaduan = request.form["jenis_pengaduan"]
     lokasi = request.form["lokasi"]
     keluhan = request.form["keluhan"]
-    status = False
     if file and allowed_file(file.filename) and nama_pengadu and jenis_pengaduan and lokasi and keluhan:
         filetype = file.filename.rsplit(".", 1)[1].lower()
         filename = f"Report_{int(datetime.now().timestamp())}.{filetype}"
         file.save(os.path.join(UPLOAD_FOLDER, filename))
-        image_url = "http://localhost:5000/images/" + filename
+        image_url = filename
         conn = db_conn()
         cur = conn.cursor()
-        cur.execute("INSERT INTO reports (user_id, nama_pengadu, jenis_pengaduan, lokasi, keluhan, image_url, status, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (user_id, nama_pengadu, jenis_pengaduan, lokasi, keluhan, image_url, status, datetime.now(), datetime.now()))
+        while True:
+            cur.execute("SELECT * FROM reports WHERE no_laporan = %s", (no_laporan,))
+            if cur.fetchone() is None:
+                break
+            else:
+                timestamp = int(datetime.now().timestamp())
+                random_number = random.randint(100, 999)
+                if jenis_pengaduan == "Perbaikan":
+                    no_laporan = f"PRB_{timestamp}_{random_number}"
+                else:
+                    no_laporan = f"PNG_{timestamp}_{random_number}"
+        cur.execute("INSERT INTO reports (user_id, no_laporan, nama_pengadu, jenis_pengaduan, lokasi, keluhan, image_url, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (user_id, no_laporan, nama_pengadu, jenis_pengaduan, lokasi, keluhan, image_url, datetime.now(), datetime.now()))
         conn.commit()
         cur.close()
         conn.close()
-        return jsonify({"message": "Pengaduan Anda Telah Kami Terima"}), 201
+        return jsonify({"message": "No Pengaduan " + no_laporan + " Telah Kami Terima"}), 201
     else:
         return jsonify({"message": "Image only"}), 400
+    
+def get_reports():
+    user = request.args.get("user")
+    conn = db_conn()
+    cur = conn.cursor()
+    if user:
+        cur.execute("SELECT id, no_laporan, status, lokasi, image_url FROM reports WHERE user_id = %s", (user,))
+    else:
+        return jsonify({"message": "User not found"}), 404
+    rows = cur.fetchall()
+    if rows:
+        reports = []
+        for row in rows:
+            reports.append({
+                "id": row[0],
+                "no_laporan": row[1],
+                "status": row[2],
+                "lokasi": row[3],
+                "image_url": row[4]
+            })
+        return jsonify(reports), 200
+    else:
+        return jsonify({"message": "Report not found"}), 404
